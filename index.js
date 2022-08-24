79bab96af94ba7da470c5aef80f75a630449ffae
mohammad-zamaniii <108420668+mohammad-zamaniii@users.noreply.github.com>
2022-08-24 16:09:37 +0300

    multiply

---
+++ index.js
@@ -0,0 +1,61 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+
+const questionEl = document.getElementById("question");
+const inputEl = document.getElementById("input");
+const formEl = document.getElementById("form");
+const scoreEl = document.getElementById("score")
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+  score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+questionEl.innerText = `what is ${num1} multiply by ${num2}?`;
+// scoreEl.innerText = `score: ${score}`;
+// questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
+});
+console.log(score,'aa')
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score))
+}
+
+
+
+
+
+// const questionEl = document.getElementById("question");
+
+// const inputEl = document.getElementById("input");
+
+// const formEl = document.getElementById("form");
+
+// const scoreEl = document.getElementById("score");
+
+// let score = JSON.parse(localStorage.getItem("score"));
+
+// if (!score) {
+//   score = 0;
+// }
+
+
+
+
+
+// function updateLocalStorage() {
+//   localStorage.setItem("score", JSON.stringify(score));
+// }
